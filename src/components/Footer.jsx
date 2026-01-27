bba1a98954970cf6821c4c0002e76592897da336
justin <jradame@gmail.com>
2026-01-27 13:35:34 -0600

    clouse

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,19 @@ function Footer() {
     <footer className="footer">
       <div className="footer-inner">
         <span>© {new Date().getFullYear()} Clouse Photography</span>
+
+        <div className="footer-credit">
+          DESIGNED &amp; CODED BY JUSTIN ADAME
+        </div>
+
         <div className="footer-links">
           {/* <a
-  href="https://www.instagram.com/thisnormallife"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Instagram
-</a> */}
-
+            href="https://www.instagram.com/thisnormallife"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a> */}
 
           <a href="mailto:hello@example.com">Email</a>
         </div>
